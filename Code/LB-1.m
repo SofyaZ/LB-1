77a8cb9f943031cc24716a80b49a932d8a89b0f4
SofyaZ <36793329+SofyaZ@users.noreply.github.com>
2018-04-09 23:18:17 +0700

    Update LB-1.m

--- Code/LB-1.m
+++ Code/LB-1.m
@@ -1,13 +1,17 @@
-f = fopen('C:\input.txt');
+f = fopen('C:\input.txt'); %input.txt
+
 for i = 1:5 %считываем информацию с файла
+    
     str = '';
     C = textscan(f, '%s', 1);
+   
     for k = 1:length(C)
         str = [str char(C{k}) ' '];
     end;
-    eval(['mass',num2str(i), ' = str2num(str);']);
+        eval(['mass',num2str(i), ' = str2num(str);']);
 end;
-X = mass1;
+
+X = mass1';
 U = mass2;
 N = mass3;
 Y = mass4;
@@ -16,90 +20,100 @@ T = mass5;
 x = length(X);
 u = length(U);
 n = length(N);
+y = length(Y);
 
-A = randn(x);
-B = randn(u, x);
-E = randn(n, x);
+A = randn(x, x);
+B = randn(x, u);
+E = randn(x, n);
 
-t = 0;
-while t < T
-    X = X*A + U*B + N*E;
-    t = t + 1;
+C = randn(y, x);
+D = randn(y, u);
+F = randn(y, n);
+
+t=0;
+for t=1:T
+    
+    N = randn(n, 1);
+    X = A*X + B*U' + E*N;
+    Y = C*X + D*U' + F*N;
+
+    Xtransf(:,t) = X;
+    Ytransf(:,t) = Y;
+
 end
 
-C = randn(x);
-D = randn(u, x);
-F = randn(n, x);
-
-Y = X*C + U*D + N*F;
 dlmwrite('output.txt', Y);
 
+%____проверка на устойчивость
 
-onematrix = eye(6,6);
+onematrix = eye(x,x);
 symbol = sym('k');
 opredelitel = det(symbol*onematrix-A);
 roots = solve(opredelitel,symbol);
 roots = double(roots);
-roots1 = abs(roots);
+
+R=real(roots);
+Im=imag(roots); 
 
 flag1=0;
 flag2=0;
+flag3=0;
 
-R=real(roots); %здесь менять имя
-Im=imag(roots); %здесь менять имя
-
-for i=1:length(R)
-if ((R(i)>-1) && (R(i)<1))
-% flag1 = flag1+1;
+for i=1:length(roots)
+if abs(R(i))<1
+    flag1 = flag1+1;
 else
-flag1=flag1+1;
+    flag3=flag3+1;
 end;
 end
-%-----------------------------------------
-if Im == 0
-    flag2=1;
-end;
 
 for i=1:length(Im)
-if ((Im(i)>-1) && (Im(i)<1))
-else
-flag2=flag2+1;
+if abs(Im(i))<1
+    flag2=flag2+1;
+else 
+    flag3=flag3+1;
 end;
 end
 
-if flag1>0 && flag2>0
-    disp('Система не устойчива')
-else
+if (flag1>0 || flag2>0) && (flag3<1)
     disp('Система устойчива')
+else
+    disp('Система не устойчива')
 end;
+
+%___время переходного процесса
 
 minrealroots = min(R);
 delta = 0.05;
 tp = (1/abs(minrealroots))*(log(1/delta))
 
+%___перерегулирование
+
 Ymax = max(Y);
-Yust = Y(1,length(Y));
+Yust = Y(length(Y),1);
 Sigma = ((Ymax-Yust)/Yust)*100
 
+%___фильтр
 
 window = 3;
 alpha=2/(window+1);
 
-Nx = length(X);
-EMAx = zeros(1, Nx);
-EMAx(1) = X(1);
-for i=2:Nx
-     EMAx(i) = alpha*X(i) + (1-alpha)*EMAx(i-1);
-end
+Ny = length(Ytransf);
+Ny1 = length(Ytransf(:,1));
+    for i=1:Ny
+        Y1=zeros(Ny1,1);
+        Y1=Ytransf(:,i);
+        
+        Filtery=zeros(1,Ny1);
+        Filtery(1)=Y1(1);
+        
+        for j=2:Ny1
+            Filtery(j) = alpha*Y1(j) + (1-alpha)*Filtery(j-1);
+            Filter=Filtery'
+        end
+        Filterytr(:,i)=Filter;
+    end
 
-YwEMA = EMAx*C + U*D + N*F;
-Ny = length(YwEMA);
-EMAy = zeros(1, Ny);
-EMAy(1) = YwEMA(1);
-for i=2:Ny
-     EMAy(i) = alpha*YwEMA(i) + (1-alpha)*EMAy(i-1);
-end
-
-plot(YwEMA);
+plot(1:T,Ytransf);
 hold on;
-plot(EMAy);
+plot(1:T, Filterytr);
